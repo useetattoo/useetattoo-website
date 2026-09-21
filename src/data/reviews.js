c9136a1a8c4172ede627cacb7e4887e2e1062c04
Hossein <fardinfard.h@gmail.com>
2026-09-21 11:57:49 -0500

    Homepage: add Google reviews section (real reviews + 4.9/Google summary + link to profile)

---
+++ src/data/reviews.js
@@ -0,0 +1,40 @@
+// Real Google reviews from the Usee Tattoo Google Business profile (verbatim excerpts).
+// Aggregate rating/count come from site.reviews; the links below point to the live profile.
+// To refresh: pull the latest reviews from the Google listing and update `items`.
+export const googleReviews = {
+  // Google place: USEE Tattoo - Studio (placeid ChIJ0bPRK6RRqEcRPq78788wb2U)
+  url: 'https://search.google.com/local/reviews?placeid=ChIJ0bPRK6RRqEcRPq78788wb2U',
+  writeUrl: 'https://search.google.com/local/writereview?placeid=ChIJ0bPRK6RRqEcRPq78788wb2U',
+  items: [
+    {
+      author: 'Rimsha Ali',
+      stars: 5,
+      text: 'I had Jvd (Javad) do my tattoo and had a great experience. He was very communicative and turned my vision to reality exactly how I had imagined it. I am definitely going to Jvd for all my future tattoos.',
+    },
+    {
+      author: 'Paul Prime',
+      stars: 5,
+      text: 'Had a very spontaneous walk in. My tattoo artist was simply amazing. From the sketch to the tattooing itself, everything smooth, friendly, welcoming, hygienic. I am more than happy with the results. Will come back for sure.',
+    },
+    {
+      author: 'TK Munyon',
+      stars: 5,
+      text: 'Javad was our artist and was awesome. Everything went smooth and he created some sick clouds. Super easy to talk to, listened to what we wanted and made suggestions. All around great experience and I recommend.',
+    },
+    {
+      author: 'Andrea Rivadossi',
+      stars: 5,
+      text: 'It was my first tattoo. The tattoo artist is really nice and a caring person. He was worried if I was suffering and if the situation was ok. I would come again.',
+    },
+    {
+      author: 'Trevor Thomas',
+      stars: 5,
+      text: 'Was in Berlin for C2C Festival and my wife and I decided to pop into USEE for an event related tattoo for the memories. Cannot say enough about the hospitality and professionalism of all the artists in the shop! Would recommend.',
+    },
+    {
+      author: 'Daniel',
+      stars: 5,
+      text: 'I was welcomed warmly right away, and the consultation on the different designs was great. I got two new little tattoos. I felt very comfortable and I am super happy with each piece. I can only recommend him.',
+    },
+  ],
+};
